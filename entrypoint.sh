d5d676a8f7ee658144b5f4e52a13eb0c363ac966
Jesse Myers <jesse.myers@gmail.com>
2019-04-20 19:29:56 -0700

    Enable DEBUG logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
-#	 --worker-class gevent \
-#	 --timeout 6 \
-#	 --graceful-timeout 6 \
 
 if [ "$1" = "server" ]; then
     exec gunicorn \
 	 --access-logfile - \
 	 --bind 0.0.0.0:80 \
+	 --log-level DEBUG \
 	 --user nobody \
 	 --group nobody \
 	 --workers 4 \
+	 --worker-class gevent \
+	 --timeout 6 \
+	 --graceful-timeout 6 \
 	 "backend.wsgi:app"
 elif [ "$1" = "shell" ]; then
     exec /bin/bash
