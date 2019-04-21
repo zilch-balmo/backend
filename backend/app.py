0820fb982c96ff74e957427e1b273fd63e163249
Jesse Myers <jesse.myers@gmail.com>
2019-04-21 13:04:18 -0700

    Use a simpler secrets manager

--- backend/app.py
+++ backend/app.py
@@ -2,15 +2,34 @@
 Create the application.
 
 """
+from json import loads
+
+from boto3 import Session
 from microcosm.api import create_object_graph
 from microcosm.loaders import load_each, load_from_environ, load_from_json_file
 from microcosm.loaders.compose import load_config_and_secrets
-from microcosm_secretsmanager.loaders.conventions import load_from_secretsmanager
 
 import backend.postgres  # noqa: F401
 import backend.routes  # noqa: F401
 import backend.stores  # noqa: F401
 from backend.config import load_default_config
+
+
+def load_secrets(metadata):
+    try:
+        session = Session()
+        secretsmanager = session("secretsmanager")
+        response = secretsmanager.get_secret_value(
+            SecretId="secrets//backend",
+            VersionStage="AWSCURENT",
+        )
+        print(response)  # noqa
+        data = loads(response["SecretString"])
+        print(data)  # noqa
+        return data.get("config", {})
+    except Exception as error:
+        print("!!!!", error)  # noqa
+        return {}
 
 
 def create_app(debug=False, testing=False, model_only=False):
@@ -22,12 +41,12 @@ def create_app(debug=False, testing=False, model_only=False):
         load_default_config,
         load_from_environ,
         load_from_json_file,
-        load_from_secretsmanager(),
+        load_secrets,
     )
 
     partitioned_loader = load_config_and_secrets(
         config=config_loader,
-        secrets=load_from_secretsmanager(),
+        secrets=load_secrets,
     )
 
     graph = create_object_graph(
@@ -36,8 +55,6 @@ def create_app(debug=False, testing=False, model_only=False):
         testing=testing,
         loader=partitioned_loader,
     )
-
-    print(graph.config.postgres)  # noqa
 
     graph.use(
         "logging",
