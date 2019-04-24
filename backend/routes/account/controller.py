102022f444d1ec2275f488b39f91f6fb10ae975a
wheremiah <jeremiah.porten@globality.com>
2019-04-24 14:12:56 -0700

    Adds logging request headers for debugging Cognito (#2)

--- backend/routes/account/controller.py
+++ backend/routes/account/controller.py
@@ -5,10 +5,12 @@ Account controller.
 from microcosm.api import binding
 from microcosm_flask.conventions.crud_adapter import CRUDStoreAdapter
 from microcosm_flask.namespaces import Namespace
+from microcosm_logging.decorators import logger
 
 from backend.models.account import Account
 
 
+@logger
 @binding("account_controller")
 class AccountController(CRUDStoreAdapter):
 
@@ -19,3 +21,8 @@ class AccountController(CRUDStoreAdapter):
             subject=Account,
             version="v1",
         )
+
+    def search(self, offset, limit, **kwargs):
+        from flask import request
+        self.logger.info(f"Request headers:\n{request.headers}")
+        return super().search(offset, limit, **kwargs)
